1894ea1891f561bf0ebcc936bd4a34a777713648
Princya Oliveira <princya.oliveira@outlook.com>
2021-08-02 22:38:56 -0300

    (02/08) iniciando projeto

---
+++ menu_horizontal/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <section id="inicio">
+        <h2>Início</h2>
+      </section>
+      <section id="cursos">
+        <h3>Cursos</h3>
+      </section>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+Seções:
+  1 - início
+  2 - cursos
+  3 - contatos
+  4 - parceiros
+  5 - missão
+  6 - rodapé - footer
+*/
