f57a9d19281acd3dbab1940b65a9940078370852
Princya Oliveira <princya.oliveira@outlook.com>
2021-08-07 13:00:46 -0300

    (07/08) criando seções

--- menu_horizontal/src/componentes/App.js
+++ menu_horizontal/src/componentes/App.js
@@ -7,8 +7,25 @@ function App() {
       <section id="inicio">
         <h2>Início</h2>
       </section>
+
       <section id="cursos">
         <h3>Cursos</h3>
+      </section>
+
+      <section id="contatos">
+        <h3>Contatos</h3>
+      </section>
+
+      <section id="parceiros">
+        <h3>Parceiros</h3>
+      </section>
+
+      <section id="missao">
+        <h3>Missão</h3>
+      </section>
+
+      <section id="sobre">
+        <h3>Sobre</h3>
       </section>
     </div>
   );
@@ -23,5 +40,5 @@ Seções:
   3 - contatos
   4 - parceiros
   5 - missão
-  6 - rodapé - footer
+  6 - sobre
 */
