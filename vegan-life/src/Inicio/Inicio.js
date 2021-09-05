7ca1f48135bf214310a615f546d7605d0b969b11
Princya Oliveira <princya.oliveira@outlook.com>
2021-09-05 14:10:54 -0300

    (05/09) configurações página inicial

--- vegan-life/src/Inicio/Inicio.js
+++ vegan-life/src/Inicio/Inicio.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import '../Inicio/Inicio.js';
 
 import './Inicio.css'
 import Logo from '../Images/logo-cabecalho.png'
+import Balao from '../Images/chat.png'
 
 export default function Inicio() {
     return(
-        <>
-
+        <>  
             <div className='inicio'>
-
+                
                 <figure>
                     <img id="logo" src={Logo} />
                 </figure>
@@ -19,8 +20,21 @@ export default function Inicio() {
                     <p class="text-3">COMIDINHAS DELICIOSAS &</p>
                     <p class="text-4">100% VEGANAS</p>
                 </div>
-              
+
+                <div>
+                    <p class="text-chat-1">10 de 10 animais</p>
+                    <p class="text-chat-2">afirmam que não</p>
+                    <p class="text-chat-3">querem ser sua</p>
+                    <p class="text-chat-4">comida</p>
+                </div>
+
+                <figure>
+                    <img id="chat" src={Balao} />
+                </figure>
             </div>
+            <footer>
+                <h3>Rodapé</h3>
+            </footer>
            
         </>
     )
