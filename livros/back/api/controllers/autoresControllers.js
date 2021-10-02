6f2e73c00ec6f891d012a29b736225d167c51315
Princya Oliveira <princya.oliveira@outlook.com>
2021-10-02 12:55:07 -0300

    (02/11) criando acessos autores

--- livros/back/api/controllers/autoresControllers.js
+++ livros/back/api/controllers/autoresControllers.js
@@ -2,7 +2,9 @@ const autoresModels = require('../models/autoresModels.js')
 
 module.exports = {
     autoresMenu,
-    autoresGetAll
+    autoresGetAll,
+    autoresGetById,
+    autoresAtivoInativo
 }
 
 function autoresMenu(req, res) {
@@ -13,13 +15,60 @@ function autoresMenu(req, res) {
 function autoresGetAll(req, res) {
     console.log('Listar autores {M O D E L}')
 
-    autoresModels.getAllAutores( function(err, resposta){
-        console.log('Retorno de autores {M O D E L}')
+    autoresModels.getAllAutores(function(err, resposta){
+        console.log('Retorno de autores {M O D E L}\n', resposta)
+
         if(err) {
             throw err;
         }
         else {
             res.json(resposta)
         }
+    })
+}
+
+function autoresGetById(req, res) {
+    console.log('Consultar autores {M O D E L}')
+    const id = req.params.codigo;
+    console.log('Parâmetro esperado: ' + id);
+
+    autoresModels.getByIdAutores(id, function(err, resposta){
+        console.log('Retorno de autores {M O D E L}\n', resposta)
+        if(err) {
+            throw err;
+        }
+        else {
+            res.json(resposta)
+        }
+    })
+}
+
+function autoresAtivoInativo(req, res) {
+    console.log('Ativar Inativar autores {M O D E L}')
+    const id = req.params.codigo;
+    console.log('Parâmetro esperado: ' + id);
+
+    autoresModels.getByIdAutores(id, function(err, resposta){
+        console.log('Retorno de autores Ativo/Inativo: ', resposta[0].aut_ativoinativo);
+        let p_ativo = resposta[0].aut_ativoinativo;
+
+        if(err) {
+            throw err;
+        }
+        else {
+            if(p_ativo == 'A') {
+                p_ativo = "I"
+            }
+            else {
+                p_ativo = "A"
+            }
+        }
+        console.log("A/I: " + p_ativo)
+        autoresModels.ativarInativar(id, p_ativo, function(err, result){
+            if(err) {
+                throw err
+            }
+            console.log("Registro Atualizado!")
+        })
     })
 }
--- livros/back/api/routes/autoresRoutes.js
+++ livros/back/api/routes/autoresRoutes.js
@@ -3,3 +3,7 @@ const controllerAutores = require('../controllers/autoresControllers.js')
 server.get('/autores', controllerAutores.autoresMenu)
 
 server.get('/autores/listar', controllerAutores.autoresGetAll)
+
+server.get('/autores/consultar/:codigo', controllerAutores.autoresGetById)
+
+server.get('/autores/ativoInativo/:codigo', controllerAutores.autoresAtivoInativo)
