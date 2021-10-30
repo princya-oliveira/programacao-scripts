199184af0f6007e575c8dbebbb954ee17e78a313
Princya Oliveira <princya.oliveira@outlook.com>
2021-10-30 12:34:51 -0300

    (30/10) alterações menu horizontal

--- livros/front/src/componentes/MenuHorizontal/MenuHorizontal.js
+++ livros/front/src/componentes/MenuHorizontal/MenuHorizontal.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import Autores from '../Autores/Autores';
-import { Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import AreaDados from './AreaDados';
 import './MenuHorizontal.css';
 
 export default function MenuHorizontal() {
     return (
-        <div>
-            <div className="menu-horizontal">
-                <nav className="navMenu">
-                    <ul>
-                        <li> <Link to="/"> Início </Link> </li>
-                        <li> <Link to="/autores"> Autores </Link> </li>
-                        <li> <Link to="/editoras"> Editoras </Link> </li>
-                        <li> <Link to="/livros"> Livros </Link> </li>
-                        <li> <Link to="/usuarios"> Usuários </Link> </li>
-                        <li> <Link to="/configuracoes"> Configurações </Link> </li>
-                    </ul>
-                </nav>
-                <Route exact path="/autores" render={(props) => <Autores />}></Route>  
+        <Router>
+            <div>
+                <div className="menu-horizontal">
+                    <nav className="navMenu">
+                        <ul>
+                            <li> <Link to="/"> Início </Link> </li>
+                            <li> <Link to="/autores"> Autores </Link> </li>
+                            <li> <Link to="/editoras"> Editoras </Link> </li>
+                            <li> <Link to="/livros"> Livros </Link> </li>
+                            <li> <Link to="/usuarios"> Usuários </Link> </li>
+                            <li> <Link to="/configuracoes"> Configurações </Link> </li>
+                        </ul>
+                    </nav>
+                    <Route exact path="/autores" render={(props) => <Autores />}></Route>  
+                </div>
             </div>
-        </div>
+        </Router>
     );
 }
 
