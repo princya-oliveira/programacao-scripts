ab2a39377e9d44c9d723427bb3978ed91265f5a7
Princya Oliveira <princya.oliveira@outlook.com>
2021-11-08 22:13:09 -0300

    (08/11) alteração autores.js

--- livros/front/src/componentes/Autores/Autores.js
+++ livros/front/src/componentes/Autores/Autores.js
@@ -26,27 +26,39 @@ function Autores() {
 
   return (
     <div id="idAutores" className="autores">
-      <h1>Autores *******************</h1>
-      <div className="autoresListar">
-        <h1> Listando Autores </h1>
+      <div id="corpo_rel">
+        <legend>Registro de Autores Cadastrados</legend>
+        <table id="tabela" className="table-striped">
+          <thead id="cabecalho_rel">
+            <tr>
+              <th id="codigo">Código</th>
+              <th id="a-i">A/I</th>
+              <th id="nome">Nome do Autor</th>
+              <th id="apelido">Apelido</th>
+              <th id="sexo">Sexo</th>
+              <th id="fone">Telefone</th>
+              <th id="e-mail">e-mail</th>
+              <th id="novo"> <a className="btn-btn-sucess btn-block" href="/autores/novo">Novo Autor</a> </th>
+            </tr>
+          </thead>
+          <tbody id="corpo_rel">
+            {evento.map((item, i) =>
+              <tr style={{textAlign: 'left'}}>
+                <th scope="row" style={{textAlign:'center'}}> {item.aut_codigo} </th>
+                <td> {item.aut_nome} </td>
+                <td> {item.aut_apelido} </td>
+                <td> {item.aut_ativoinativo} </td>
+                <td> {item.aut_sexo} </td>
+                <td> {item.aut_telefone} </td>
+                <td> {item.aut_email} </td>
 
-        <ul className="list_group">
-          {evento.map((item, i) =>
-            <li className="list-group-item" key={i}>
-              <span> Código: {item.aut_codigo} </span>
-              <span> Nome: {item.aut_nome} </span>
-              <span> Apelido: {item.aut_apelido} </span>
-              <span> A/I: {item.aut_ativoinativo} </span>
-              <span> Sexo: {item.aut_sexo} </span>
-              <span> Telefone: {item.aut_telefone} </span>
-              <span> E-Mail: {item.aut_email} </span>
-            </li>
-          )
-          }
-        </ul>
+                <td id="editar"> <a className="btn btn-primary btn-block" href={`/autores/editar/${item.aut_codigo}`}> Editar </a></td>
+                <td id="ativar"> <a className="btn btn-danger btn-block" href={`/autores/ativoInativo/${item.aut_codigo}`}> Inativar </a></td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
-
-
     </div>
   );
 }
